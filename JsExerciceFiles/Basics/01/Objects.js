ed0535a6e51b26937d6f163cfeeeb491082c24fe
williamlinarescalderon <william.calderon.bit1@gmail.com>
2023-05-12 15:10:30 -0700

    first

---
+++ JsExerciceFiles/Basics/01/Objects.js
@@ -0,0 +1,15 @@
+//example
+
+var ourDog = {
+    "name" : "dexter",
+    "legs" : "4",
+    "age"  : "5",
+    "friends" : ["everything"]
+};
+
+//acces to the properties 
+
+var nombre =  ourDog.name;
+var age = ourDog.age;
+
+console.log(nombre,age);
