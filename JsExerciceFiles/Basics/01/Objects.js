64cf50f9f22201de170045905a31cf5c5c10a147
williamlinarescalderon <william.calderon.bit1@gmail.com>
2023-05-12 15:18:01 -0700

    second

--- JsExerciceFiles/Basics/01/Objects.js
+++ JsExerciceFiles/Basics/01/Objects.js
@@ -4,12 +4,15 @@ var ourDog = {
     "name" : "dexter",
     "legs" : "4",
     "age"  : "5",
-    "friends" : ["everything"]
+    "friends" : ["everything"],
+    "the food": "meat"
 };
 
 //acces to the properties 
+//properties witout space into the name, you can access with the dot 
+var nombre  =   ourDog.name;
+var age     =   ourDog.age;
+//properties with empty space into the declaration, you can acces using brackets
+var comida  =   ourDog["the food"]; 
 
-var nombre =  ourDog.name;
-var age = ourDog.age;
-
-console.log(nombre,age);
+console.log(nombre,age, comida);
